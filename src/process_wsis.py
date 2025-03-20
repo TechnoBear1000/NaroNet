707bb1a43e62cff6e100c2f4d39374500112b4b5
Erik Ylipää <erik.ylipaa@gmail.com>
2025-03-20 13:29:45 +0100

    Added separate file for preparing WSIs

---
+++ src/process_wsis.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+from NaroNet.utils.DatasetParameters import parameters
+from NaroNet.Patch_Contrastive_Learning.patch_contrastive_learning import patch_contrastive_learning
+from NaroNet.Patch_Contrastive_Learning.preprocess_images import preprocess_wsis
+from NaroNet.architecture_search.architecture_search import architecture_search
+from NaroNet.NaroNet import run_NaroNet
+from NaroNet.NaroNet_dataset import get_BioInsights
+
+def main(path):
+    # Select Experiment parameters
+    params = parameters(path, 'Value')
+    possible_params = parameters(path, 'Object')
+    best_params = parameters(path, 'Index')    
+
+    # Preprocess Images
+    preprocess_wsis(path)
+
+
+
+if __name__ == "__main__":
+    path = 'dataset/'
+    main(path)
+ 
